37f421181d63838b1b7916034b92c7b0838fed48
ian <ian@iank.org>
2010-09-05 16:08:29 -0400

    Primary keys are good

--- lib/PicBot/DB/Main/Result/Tags.pm
+++ lib/PicBot/DB/Main/Result/Tags.pm
@@ -3,7 +3,7 @@ use base qw/DBIx::Class/;
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('tags');
 __PACKAGE__->add_columns("pid", "who", "tag");
-__PACKAGE__->add_unique_constraint(['pid', 'tag']);
+__PACKAGE__->set_primary_key('pid','tag');
 __PACKAGE__->belongs_to("pic", "PicBot::DB::Main::Result::Pdb", {pid=>"pid"});
 
 1
