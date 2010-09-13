6df78b5497d79316013884f07a60fe05bddcc5b4
Ryan Voots <simcop2387@simcop2387.info>
2010-09-13 19:32:13 -0400

    fucking fast searches now, FUCKING FAST

--- lib/PicBot/DB/Main/Result/Tags.pm
+++ lib/PicBot/DB/Main/Result/Tags.pm
@@ -4,6 +4,6 @@ __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('tags');
 __PACKAGE__->add_columns("pid", "who", "tag");
 __PACKAGE__->set_primary_key('pid','tag');
-__PACKAGE__->belongs_to("pic", "PicBot::DB::Main::Result::Pdb", {pid=>"pid"});
+__PACKAGE__->belongs_to("pic", "PicBot::DB::Main::Result::Pdb", {pid=>"pid"}, {join_type => "INNER"});
 
 1
